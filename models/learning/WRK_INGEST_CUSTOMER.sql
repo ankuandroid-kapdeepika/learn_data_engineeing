9fa8e4ca55ee0afa81dbf0d27164e1c3d0fa1dbd
prateek.cts@gmail.com <prateek.cts@gmail.com>
2025-06-07 04:02:28 +0000

    .

--- models/learning/WRK_INGEST_CUSTOMER.sql
+++ models/learning/WRK_INGEST_CUSTOMER.sql
@@ -2,4 +2,4 @@ select
 C_CUSTKEY,C_NAME,C_ADDRESS,C_NATIONKEY,C_PHONE
 ,C_ACCTBAL
 ,C_MKTSEGMENT
-,C_COMMENT from {{ source("snowflake_sample", "CUSTOMER") }} 
+,C_COMMENT from {{ source("snowflake_sample", "CUSTOMER") }} limit 10
